4271a7978e61d4a0ea168dedf2e10e5b70cee12e
Omolara Adebayo Wahab <omolaraadebayowahab@gmail.com>
2023-01-30 02:13:25 +0100

    new programming exercise on object and class

--- Functions/Current_date_and_time/cmake-build-debug/cmake_install.cmake
+++ Functions/Current_date_and_time/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/THOTH0101/CLionProjects/Current_date_and_time
+# Install script for directory: /home/THOTH0101/c_plus_plus_projects/Functions/Current_date_and_time
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/THOTH0101/CLionProjects/Current_date_and_time/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/THOTH0101/c_plus_plus_projects/Functions/Current_date_and_time/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
